a8a313a0e585204315fad683475edd6e0b2d33d4
prb71 <96513118+prb71@users.noreply.github.com>
2023-04-22 20:00:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-docker run -itd --name pavan nginx
+operation=$1
+repo=https://github.com/prb71/bitcot.git
+
+
+create () {
+cd /home/ec2-user
+rm -rf ./bitcot
+git clone $repo
+cd ./bitcot
+sudo docker-compose build --no-cache && sudo docker-compose up -d 
+}
+
+destroy () {
+cd /home/ec2-user/bitcot
+echo "docker compose is destroying"
+sudo docker-compose down
+echo "Removing images from local system"
+docker rmi -f $(docker images -aq)
+fi
+}
+
+case ${operation} in
+  create) create ;;
+    destroy) destroy ;;
+       *) echo "Unknown action ${operation}" ;;
+esac
